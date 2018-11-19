87c43c314e66fbc24a7c8db112fe3a3375eef282
HiroakiFuse <33142304+HiroakiFuse@users.noreply.github.com>
2018-11-19 09:34:00 +0900

    Update App.vue

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -73,12 +73,12 @@
 import Firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyB8ZVjhyO5CvSpO4Iu6n0MmmsO_uOLPyPs",
-  authDomain: "fk6-co.firebaseapp.com",
-  databaseURL: "https://fk6-co.firebaseio.com",
-  projectId: "fk6-co",
-  storageBucket: "fk6-co.appspot.com",
-  messagingSenderId: "810812087591"
+  apiKey: "###",
+  authDomain: "###",
+  databaseURL: "###",
+  projectId: "###",
+  storageBucket: "###",
+  messagingSenderId: "###"
 }
 
 const app = Firebase.initializeApp(config);
